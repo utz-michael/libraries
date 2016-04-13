046e33ae5fb51ea57ab688c818ef85a5362ba2e6
Michael Utz <utzm@kabelbw.de>
2016-04-13 08:09:32 +0200

    Ethernet gegen Ethernet 2 ausgetauscht
    Ethernet old ist das original in ligt im root

--- Ethernet/src/Ethernet.cpp
+++ Ethernet/src/Ethernet.cpp
@@ -1,25 +1,83 @@
-#include "utility/w5100.h"
+/*
+ modified 12 Aug 2013
+ by Soohwan Kim (suhwan@wiznet.co.kr)
+*/
 #include "Ethernet.h"
 #include "Dhcp.h"
 
 // XXX: don't make assumptions about the value of MAX_SOCK_NUM.
-uint8_t EthernetClass::_state[MAX_SOCK_NUM] = { 
-  0, 0, 0, 0 };
-uint16_t EthernetClass::_server_port[MAX_SOCK_NUM] = { 
-  0, 0, 0, 0 };
+uint8_t EthernetClass::_state[MAX_SOCK_NUM] = { 0, };
+uint16_t EthernetClass::_server_port[MAX_SOCK_NUM] = { 0, };
 
-int EthernetClass::begin(uint8_t *mac_address)
+
+
+#if defined(WIZ550io_WITH_MACADDRESS)
+int EthernetClass::begin(void)
 {
-  static DhcpClass s_dhcp;
-  _dhcp = &s_dhcp;
-
+  byte mac_address[6] ={0,};
+  _dhcp = new DhcpClass();
 
   // Initialise the basic info
   W5100.init();
-  SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
+  W5100.setIPAddress(IPAddress(0,0,0,0).raw_address());
+  W5100.getMACAddress(mac_address);
+  
+  // Now try to get our config info from a DHCP server
+  int ret = _dhcp->beginWithDHCP(mac_address);
+  if(ret == 1)
+  {
+    // We've successfully found a DHCP server and got our configuration info, so set things
+    // accordingly
+    W5100.setIPAddress(_dhcp->getLocalIp().raw_address());
+    W5100.setGatewayIp(_dhcp->getGatewayIp().raw_address());
+    W5100.setSubnetMask(_dhcp->getSubnetMask().raw_address());
+    _dnsServerAddress = _dhcp->getDnsServerIp();
+  }
+
+  return ret;
+}
+
+void EthernetClass::begin(IPAddress local_ip)
+{
+  // Assume the DNS server will be the machine on the same network as the local IP
+  // but with last octet being '1'
+  IPAddress dns_server = local_ip;
+  dns_server[3] = 1;
+  begin(local_ip, dns_server);
+}
+
+void EthernetClass::begin(IPAddress local_ip, IPAddress dns_server)
+{
+  // Assume the gateway will be the machine on the same network as the local IP
+  // but with last octet being '1'
+  IPAddress gateway = local_ip;
+  gateway[3] = 1;
+  begin(local_ip, dns_server, gateway);
+}
+
+void EthernetClass::begin(IPAddress local_ip, IPAddress dns_server, IPAddress gateway)
+{
+  IPAddress subnet(255, 255, 255, 0);
+  begin(local_ip, dns_server, gateway, subnet);
+}
+
+void EthernetClass::begin(IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet)
+{
+  W5100.init();
+  W5100.setIPAddress(local_ip.raw_address());
+  W5100.setGatewayIp(gateway.raw_address());
+  W5100.setSubnetMask(subnet.raw_address());
+  _dnsServerAddress = dns_server;
+}
+#else
+int EthernetClass::begin(uint8_t *mac_address)
+{
+  _dhcp = new DhcpClass();
+
+  // Initialise the basic info
+  W5100.init();
   W5100.setMACAddress(mac_address);
   W5100.setIPAddress(IPAddress(0,0,0,0).raw_address());
-  SPI.endTransaction();
 
   // Now try to get our config info from a DHCP server
   int ret = _dhcp->beginWithDHCP(mac_address);
@@ -27,11 +85,9 @@ int EthernetClass::begin(uint8_t *mac_address)
   {
     // We've successfully found a DHCP server and got our configuration info, so set things
     // accordingly
-    SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
     W5100.setIPAddress(_dhcp->getLocalIp().raw_address());
     W5100.setGatewayIp(_dhcp->getGatewayIp().raw_address());
     W5100.setSubnetMask(_dhcp->getSubnetMask().raw_address());
-    SPI.endTransaction();
     _dnsServerAddress = _dhcp->getDnsServerIp();
   }
 
@@ -65,14 +121,14 @@ void EthernetClass::begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dn
 void EthernetClass::begin(uint8_t *mac, IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet)
 {
   W5100.init();
-  SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
   W5100.setMACAddress(mac);
   W5100.setIPAddress(local_ip.raw_address());
   W5100.setGatewayIp(gateway.raw_address());
   W5100.setSubnetMask(subnet.raw_address());
-  SPI.endTransaction();
   _dnsServerAddress = dns_server;
 }
+
+#endif
 
 int EthernetClass::maintain(){
   int rc = DHCP_CHECK_NONE;
@@ -86,11 +142,9 @@ int EthernetClass::maintain(){
       case DHCP_CHECK_RENEW_OK:
       case DHCP_CHECK_REBIND_OK:
         //we might have got a new IP.
-        SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
         W5100.setIPAddress(_dhcp->getLocalIp().raw_address());
         W5100.setGatewayIp(_dhcp->getGatewayIp().raw_address());
         W5100.setSubnetMask(_dhcp->getSubnetMask().raw_address());
-        SPI.endTransaction();
         _dnsServerAddress = _dhcp->getDnsServerIp();
         break;
       default:
@@ -104,27 +158,21 @@ int EthernetClass::maintain(){
 IPAddress EthernetClass::localIP()
 {
   IPAddress ret;
-  SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
   W5100.getIPAddress(ret.raw_address());
-  SPI.endTransaction();
   return ret;
 }
 
 IPAddress EthernetClass::subnetMask()
 {
   IPAddress ret;
-  SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
   W5100.getSubnetMask(ret.raw_address());
-  SPI.endTransaction();
   return ret;
 }
 
 IPAddress EthernetClass::gatewayIP()
 {
   IPAddress ret;
-  SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
   W5100.getGatewayIp(ret.raw_address());
-  SPI.endTransaction();
   return ret;
 }
 
--- Ethernet/src/utility/w5200.cpp
+++ Ethernet/src/utility/w5200.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 by Cristian Maglie <c.maglie@bug.st>
+ * Copyright (c) 2013 by WIZnet <support@wiznet.co.kr>
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of either the GNU General Public License version 2
@@ -9,13 +9,11 @@
 
 #include <stdio.h>
 #include <string.h>
-
 #include "utility/w5100.h"
 
-#if defined(W5100_ETHERNET_SHIELD)
-
-// W5100 controller instance
-W5100Class W5100;
+#if defined(W5200_ETHERNET_SHIELD)
+// W5200 controller instance
+W5200Class W5100;
 
 #define SPI_CS 10
 
@@ -23,10 +21,11 @@ W5100Class W5100;
 #define TX_BUF 0x1100
 #define RX_BUF (TX_BUF + TX_RX_MAX_BUF_SIZE)
 
-#define TXBUF_BASE 0x4000
-#define RXBUF_BASE 0x6000
 
-void W5100Class::init(void)
+#define TXBUF_BASE 0x8000
+#define RXBUF_BASE 0xC000
+
+void W5200Class::init(void)
 {
   delay(300);
 
@@ -36,20 +35,26 @@ void W5100Class::init(void)
 #else
   SPI.begin(SPI_CS);
   // Set clock to 4Mhz (W5100 should support up to about 14Mhz)
-  SPI.setClockDivider(SPI_CS, 21);
+//  SPI.setClockDivider(SPI_CS, 21);
+//  SPI.setClockDivider(SPI_CS, 6); // 14 Mhz, ok  
+//  SPI.setClockDivider(SPI_CS, 3); // 28 Mhz, ok 
+  SPI.setClockDivider(SPI_CS, 2); // 42 Mhz, ok 
   SPI.setDataMode(SPI_CS, SPI_MODE0);
 #endif
+  
   writeMR(1<<RST);
-  writeTMSR(0x55);
-  writeRMSR(0x55);
-
+  
+  for (int i=0; i<MAX_SOCK_NUM; i++) {
+    write((0x4000 + i * 0x100 + 0x001F), 2);
+    write((0x4000 + i * 0x100 + 0x001E), 2);
+  }
   for (int i=0; i<MAX_SOCK_NUM; i++) {
     SBASE[i] = TXBUF_BASE + SSIZE * i;
     RBASE[i] = RXBUF_BASE + RSIZE * i;
   }
 }
 
-uint16_t W5100Class::getTXFreeSize(SOCKET s)
+uint16_t W5200Class::getTXFreeSize(SOCKET s)
 {
   uint16_t val=0, val1=0;
   do {
@@ -61,7 +66,7 @@ uint16_t W5100Class::getTXFreeSize(SOCKET s)
   return val;
 }
 
-uint16_t W5100Class::getRXReceivedSize(SOCKET s)
+uint16_t W5200Class::getRXReceivedSize(SOCKET s)
 {
   uint16_t val=0,val1=0;
   do {
@@ -74,13 +79,13 @@ uint16_t W5100Class::getRXReceivedSize(SOCKET s)
 }
 
 
-void W5100Class::send_data_processing(SOCKET s, const uint8_t *data, uint16_t len)
+void W5200Class::send_data_processing(SOCKET s, const uint8_t *data, uint16_t len)
 {
   // This is same as having no offset in a call to send_data_processing_offset
   send_data_processing_offset(s, 0, data, len);
 }
 
-void W5100Class::send_data_processing_offset(SOCKET s, uint16_t data_offset, const uint8_t *data, uint16_t len)
+void W5200Class::send_data_processing_offset(SOCKET s, uint16_t data_offset, const uint8_t *data, uint16_t len)
 {
   uint16_t ptr = readSnTX_WR(s);
   ptr += data_offset;
@@ -103,7 +108,7 @@ void W5100Class::send_data_processing_offset(SOCKET s, uint16_t data_offset, con
 }
 
 
-void W5100Class::recv_data_processing(SOCKET s, uint8_t *data, uint16_t len, uint8_t peek)
+void W5200Class::recv_data_processing(SOCKET s, uint8_t *data, uint16_t len, uint8_t peek)
 {
   uint16_t ptr;
   ptr = readSnRX_RD(s);
@@ -115,7 +120,7 @@ void W5100Class::recv_data_processing(SOCKET s, uint8_t *data, uint16_t len, uin
   }
 }
 
-void W5100Class::read_data(SOCKET s, volatile uint16_t src, volatile uint8_t *dst, uint16_t len)
+void W5200Class::read_data(SOCKET s, volatile uint16_t src, volatile uint8_t *dst, uint16_t len)
 {
   uint16_t size;
   uint16_t src_mask;
@@ -136,94 +141,118 @@ void W5100Class::read_data(SOCKET s, volatile uint16_t src, volatile uint8_t *ds
 }
 
 
-uint8_t W5100Class::write(uint16_t _addr, uint8_t _data)
+uint8_t W5200Class::write(uint16_t _addr, uint8_t _data)
 {
 #if defined(ARDUINO_ARCH_AVR)
   setSS();  
-  SPI.transfer(0xF0);
   SPI.transfer(_addr >> 8);
   SPI.transfer(_addr & 0xFF);
+  SPI.transfer(0x80);
+  SPI.transfer(0x01); 
   SPI.transfer(_data);
   resetSS();
 #else
-  SPI.transfer(SPI_CS, 0xF0, SPI_CONTINUE);
   SPI.transfer(SPI_CS, _addr >> 8, SPI_CONTINUE);
   SPI.transfer(SPI_CS, _addr & 0xFF, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, 0x80, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, 0x01, SPI_CONTINUE);
   SPI.transfer(SPI_CS, _data);
 #endif
+  
   return 1;
 }
 
-uint16_t W5100Class::write(uint16_t _addr, const uint8_t *_buf, uint16_t _len)
+uint16_t W5200Class::write(uint16_t _addr, const uint8_t *_buf, uint16_t _len)
 {
-  for (uint16_t i=0; i<_len; i++)
-  {
+    if (_len == 0) //Fix: a write request with _len == 0 hangs the W5200
+        return 0;
+        
 #if defined(ARDUINO_ARCH_AVR)
-    setSS();    
-    SPI.transfer(0xF0);
+    setSS();
     SPI.transfer(_addr >> 8);
     SPI.transfer(_addr & 0xFF);
-    _addr++;
+    SPI.transfer((0x80 | ((_len & 0x7F00) >> 8)));
+    SPI.transfer(_len & 0x00FF);
+
+  for (uint16_t i=0; i<_len; i++)
+  {
     SPI.transfer(_buf[i]);
+  }
     resetSS();
 #else
-	SPI.transfer(SPI_CS, 0xF0, SPI_CONTINUE);
-	SPI.transfer(SPI_CS, _addr >> 8, SPI_CONTINUE);
-	SPI.transfer(SPI_CS, _addr & 0xFF, SPI_CONTINUE);
-	SPI.transfer(SPI_CS, _buf[i]);
-    _addr++;
-#endif
+  SPI.transfer(SPI_CS, _addr >> 8, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, _addr & 0xFF, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, (0x80 | ((_len & 0x7F00) >> 8)), SPI_CONTINUE);
+  SPI.transfer(SPI_CS, _len & 0x00FF, SPI_CONTINUE);
+
+  for (uint16_t i=0; i<(_len-1); i++)
+  {
+	SPI.transfer(SPI_CS, _buf[i], SPI_CONTINUE);
   }
+	SPI.transfer(SPI_CS, _buf[_len-1]);
+  
+#endif    
+    
   return _len;
 }
 
-uint8_t W5100Class::read(uint16_t _addr)
+uint8_t W5200Class::read(uint16_t _addr)
 {
+
 #if defined(ARDUINO_ARCH_AVR)
   setSS();  
-  SPI.transfer(0x0F);
   SPI.transfer(_addr >> 8);
   SPI.transfer(_addr & 0xFF);
+  SPI.transfer(0x00);
+  SPI.transfer(0x01); 
   uint8_t _data = SPI.transfer(0);
   resetSS();
 #else
-  SPI.transfer(SPI_CS, 0x0F, SPI_CONTINUE);
   SPI.transfer(SPI_CS, _addr >> 8, SPI_CONTINUE);
   SPI.transfer(SPI_CS, _addr & 0xFF, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, 0x00, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, 0x01, SPI_CONTINUE);
   uint8_t _data = SPI.transfer(SPI_CS, 0);
-#endif
+#endif  
   return _data;
 }
 
-uint16_t W5100Class::read(uint16_t _addr, uint8_t *_buf, uint16_t _len)
+uint16_t W5200Class::read(uint16_t _addr, uint8_t *_buf, uint16_t _len)
 {
-  for (uint16_t i=0; i<_len; i++)
-  {
+
 #if defined(ARDUINO_ARCH_AVR)
     setSS();
-    SPI.transfer(0x0F);
     SPI.transfer(_addr >> 8);
     SPI.transfer(_addr & 0xFF);
-    _addr++;
+    SPI.transfer((0x00 | ((_len & 0x7F00) >> 8)));
+    SPI.transfer(_len & 0x00FF);
+
+  for (uint16_t i=0; i<_len; i++)
+  {
     _buf[i] = SPI.transfer(0);
+
+  }
     resetSS();
 #else
-	SPI.transfer(SPI_CS, 0x0F, SPI_CONTINUE);
-	SPI.transfer(SPI_CS, _addr >> 8, SPI_CONTINUE);
-	SPI.transfer(SPI_CS, _addr & 0xFF, SPI_CONTINUE);
-    _buf[i] = SPI.transfer(SPI_CS, 0);
-    _addr++;
-#endif
+  SPI.transfer(SPI_CS, _addr >> 8, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, _addr & 0xFF, SPI_CONTINUE);
+  SPI.transfer(SPI_CS, (0x00 | ((_len & 0x7F00) >> 8)), SPI_CONTINUE);
+  SPI.transfer(SPI_CS, _len & 0x00FF, SPI_CONTINUE);
+  for (uint16_t i=0; i<(_len-1); i++)
+  {
+    _buf[i] = SPI.transfer(SPI_CS, 0, SPI_CONTINUE);
   }
+    _buf[_len-1] = SPI.transfer(SPI_CS, 0);
+  
+#endif    
   return _len;
 }
 
-void W5100Class::execCmdSn(SOCKET s, SockCMD _cmd) {
+void W5200Class::execCmdSn(SOCKET s, SockCMD _cmd) {
   // Send command to socket
   writeSnCR(s, _cmd);
   // Wait for command to complete
   while (readSnCR(s))
     ;
 }
-
 #endif
